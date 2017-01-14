c5336a6873aaf2ee7d3c47a5d180f31bb7ae38b5
tantan0813 <1418346401@qq.com>
2017-01-14 09:59:38 +0800

    2333

--- JSknowledge-point/js/es6/arrayandregexp.js
+++ JSknowledge-point/js/es6/arrayandregexp.js
@@ -51,6 +51,19 @@ window.onload=function(){
             })
             console.log(arr4);
         }
+        console.log(
+            arr1.findIndex(function(a){
+            a == 10;//findindex测试每一个数字是否等于1，有的话返回该值的索引值.
+            // a == 1000;//findindex测试每一个数字是否等于1000，没有的话返回-1.
+        })
+        );
+        arr1.fill(8,2,9);
+        console.log(arr1);
+
+        var arr_05 = arr1.keys();
+        console.log(arr_05.next().value,arr_05.next().value,arr_05.next().value);
+        console.log(arr_05.next().value,arr_05.next().value,arr_05.next().value);
+
     };
     color();//rgb随机取色。
     function selectfrom(lowValue, highValue) {
@@ -59,15 +72,36 @@ window.onload=function(){
     };
     function color(){
         var target = Array.prototype.slice.call(document.getElementsByClassName("boll"));
+        var target_01 = Array.from(target);
+        console.log(target_01 instanceof Array);
         console.log(target instanceof Array);
         target.forEach(function(item){
             item.style.background= "rgb("+selectfrom(0,255) +","+selectfrom(0,255)+","+selectfrom(0,255)+")";
         })
+    }
 
+    //reg正则
+    regexp();
+    function regexp(){
+        var str = "javascript",
+            d = "33 44 55 jj kk 11 ll",
+            c ;
+        // i：执行不区分大小写的匹配
+        // g：执行一个全局匹配，简言之，即找到所有的匹配，而不是在找到第一个之后就停止
+        // m：多行匹配模式，^匹配一行的开头和字符串的开头，$匹配行的结尾和字符串的结尾
+        console.log(str.search(/asc/i));//search返回第一个子串匹配的位置
+        console.log(d.match(/\d+/g),d.match(/\d+/g) instanceof Array);//match返回所有符合正则的子串数组
+        c =  d.split(" ");
+        console.log(c,c instanceof Array);//split()按一定规则把字符串分割成数组
 
-
-
-
+        //exec(),参数为字符串，类似于match，对字符串执行正则表达式，返回所有符合正则的子串数组
+        var text = "JavaScript is more fun than Java!"
+        var p = /javas/gi,r;
+        while((r = p.exec(text)) != null){
+           console.log(r,"lastIndex:"+p.lastIndex);
+        }
+        //test(),参数为字符串，返回true\\false
+        console.log(p.test("jiavaspcrit"));
     }
 
 
