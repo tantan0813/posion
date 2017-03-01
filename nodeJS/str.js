7ed210d1ae95ca85e3b6135ff07638ad9924a140
tantan0813 <1418346401@qq.com>
2017-03-01 11:55:10 +0800

    22

--- nodeJS/str.js
+++ nodeJS/str.js
@@ -741,7 +741,83 @@ rl.on("line",function(a){
     //     return result.toString().replace(/,/ig,"\n");
     // }
 
-    //24点游戏
+    //24点游戏×
+
+    // 梅花桩
+
+    // if(data.length==2){
+    //     console.log(play34(data));
+    // }
+    // function play34(arr){
+    //     var n = parseInt(arr[0]);
+    //     var ar = arr[1].split(" ");
+    //     var result = [];
+    //     for(var i=0,len=ar.length;i<len;i++){
+    //         var n=1;
+    //         var mid = ar.slice(i);
+    //         var s =[];
+    //         mid.forEach(function(a){
+    //             if(s.indexOf(a)==-1){
+    //                 s.push(a);
+    //             }
+    //         });
+    //         for(var j=0,l=s.length;j<l;j++){
+    //             if(s[j]>ar[i]){
+    //                 n++;
+    //             }
+    //         }
+    //         result.push(n);
+    //     }
+    //     result.sort(function(a,b){
+    //         return b-a;
+    //     });
+    //     return result[0];
+    // }
+
+    // 矩形乘法×
+
+    // 题目描述
+    // 数据表记录包含表索引和数值，请对表索引相同的记录进行合并，即将相同索引的数值进行求和运算，输出按照key值升序进行输出。
+
+// 输入描述:
+//     先输入键值对的个数
+//     然后输入成对的index和value值，以空格隔开
+
+//     输出描述:
+        // 输出合并后的键值对（多行）
+
+// 输入例子:
+//     4
+//     0 1
+//     0 2
+//     1 2
+//     3 4
+
+    // 输出例子:
+    //     0 3
+    //     1 2
+    //     3 4
+
+
+    function play35(arr){
+        var n = parseInt(arr[0]);
+        var arr1 = arr.slice(1);
+        for(var i=0,len=arr1.length;i<len;i++){
+            var mid = arr1[i].split(" ");
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 })
 
 
