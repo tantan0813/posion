fe47a6729da40c56126306e51d1b0f8c24186e96
tantan0813 <1418346401@qq.com>
2017-03-16 08:24:03 +0800

    222

--- nodeJS/offer.js
+++ nodeJS/offer.js
@@ -727,29 +727,18 @@ rl.on("line",function(a){
     // 有这样一道智力题：“某商店规定：三个空汽水瓶可以换一瓶汽水。小张手上有十个空汽水瓶，她最多可以换多少瓶汽水喝？”答案是5瓶，
     // 方法如下：先用9个空瓶子换3瓶汽水，喝掉3瓶满的，喝完以后4个空瓶子，用3个再换一瓶，喝掉这瓶满的，这时候剩2个空瓶子。
     // 然后你让老板先借给你一瓶汽水，喝掉这瓶满的，喝完以后用3个空瓶子换一瓶满的还给老板。如果小张手上有n个空汽水瓶，最多可以换多少瓶汽水喝？
-    // 输入描述:
-    // 输入文件最多包含10组测试数据，每个数据占一行，仅包含一个正整数n（1<=n<=100），表示小张手上的空汽水瓶数。n=0表示输入结束，你的程序不应当处理这一行。
-    // 输出描述:
-    // 对于每组测试数据，输出一行，表示最多可以喝的汽水瓶数。如果一瓶也喝不到，输出0。
-    // 输入例子:
-    // 3
-    // 10
-    // 81
-    // 0
-    // 输出例子:
-    // 1
-    // 5
-    // 40
-    k26(a);
+
     function k26(a){
        data.push(a);
        var re = [];
-       data.forEach(function(b){
-            if(b==0){
-                // console.log(data.slice(0,data.length-1));
-                l(data.slice(0,data.length-1));
-            }
-       })
+        l(data);
+       // data.forEach(function(b){
+       //      if(b==0){
+       //          // console.log(data.slice(0,data.length-1));
+       //          l(data.slice(0,data.length-1));
+       //          return false;
+       //      }
+       // });
         function l(arr){
            arr.forEach(function(c){
                var n = parseInt(c);
@@ -757,20 +746,52 @@ rl.on("line",function(a){
                while(n>=2){
                    if(n==2){
                        num++;
+                       break;
                    }else{
                        var k = n%3;
                        var d = Math.floor(n/3);
-                       num+=d;
-                        n=0;
+                        num+=d;
                         n=k+d;
                    };
                }
-               console.log(num);
                re.push(num);
-           })
-            // console.log(re.toString().replace(/,/g,"\n"));
+           });
+            console.log(re.toString().replace(/,/g,"\n"));
         }
     }
+
+
+    function k27(a){
+        data.push(a);
+        var n = parseInt(data[0]);
+        if(data.length>n){
+            var arr = data.slice(1);
+            var re = [];
+            arr.forEach(function(b){
+                if(re.indexOf(b)==-1){
+                    re.push(b);
+                }
+            })
+            console.log(re.sort(function(b,c){return b-c;}).toString().replace(/,/g,"\n"));
+        }
+    }
+
+
+
+    // 写出一个程序，接受一个十六进制的数值字符串，输出该数值的十进制字符串。（多组同时输入 ）
+    // 输入描述:
+    // 输入一个十六进制的数值字符串。
+    // 输出描述:
+    // 输出该数值的十进制字符串。
+    // 输入例子:
+    // 0xA
+    // 输出例子:
+    //     10
+    k28(a);
+    function k28(a){
+        console.log(parseInt(a,16));
+    }
+
 });
 
 
