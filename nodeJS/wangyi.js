6ca5cf8afca0bd15b1ae3094bff9ec90bd717c4e
tantan0813 <1418346401@qq.com>
2017-03-24 21:50:58 +0800

    000

--- nodeJS/wangyi.js
+++ nodeJS/wangyi.js
@@ -89,8 +89,6 @@ rl.on("line",function(a) {
        }
        console.log(count+"x");
     }
-
-
     // 买颜料
     function k5(a){
         data.push(a);
@@ -108,6 +106,44 @@ rl.on("line",function(a) {
                }
            }
            console.log(arr.length);
+        }
+    }
+
+    // 一个袋子里面有n个球，每个球上面都有一个号码(拥有相同号码的球是无区别的)。
+    // 如果一个袋子是幸运的当且仅当所有球的号码的和大于所有球的号码的积。
+    // 例如：如果袋子里面的球的号码是{1, 1, 2, 3}，
+    // 这个袋子就是幸运的，因为1 + 1 + 2 + 3 > 1 * 1 * 2 * 3
+    // 你可以适当从袋子里移除一些球(可以移除0个,但是别移除完)，要使移除后的袋子是幸运的。
+    // 现在让你编程计算一下你可以获得的多少种不同的幸运的袋子。
+    // 输入描述:
+    // 第一行输入一个正整数n(n ≤ 1000)
+    // 第二行为n个数正整数xi(xi ≤ 1000)
+    // 输出描述:
+    //     输出可以产生的幸运的袋子数
+    // 输入例子:
+    //     3
+    // 1 1 1
+    // 输出例子:
+    //     2
+    k6(a);
+    function k6(a){
+        data.push(a);
+        if(data.length>1){
+            var arr = data[1].split(" ");
+
+        }
+        function p(s){
+            var arr = s.split(",");
+            var sum=0,su=1;
+            arr.forEach(function(b){
+                sum+=parseInt(b);
+                su*=parseInt(b);
+            })
+            if(sum>su){
+                return true;
+            }else{
+                return false;
+            }
         }
     }
 });
